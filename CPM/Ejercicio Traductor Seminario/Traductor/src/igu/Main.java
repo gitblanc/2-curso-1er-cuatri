b3cfcd4b14e4d8d9462d800662f566dcc73cc06d
Eduardo Blanco Bielsa <blanc@MacBook-Pro-de-Eduardo.local>
2021-11-23 22:59:45 +0100

    ej seminario bien

---
+++ CPM/Ejercicio Traductor Seminario/Traductor/src/igu/Main.java
@@ -0,0 +1,20 @@
+package igu;
+
+import java.awt.EventQueue;
+
+public class Main {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPrincipal frame = new VentanaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
